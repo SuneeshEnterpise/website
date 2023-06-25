8d463e6a0949162db9bbaa9f8be3a4f49ecb94b8
suneeshenterprise <suneeshenterprise03@gmail.com>
2023-06-25 22:09:23 +0530

    12000

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav');
-  const navLinks = document.querySelectorAll('.nav li');
-  
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-
-    navLinks.forEach((link, index) => {
-      if(link.style.animation){
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-      }
-    });
-
-    
-  });
+const add = (a, b) => {
+	return a + b
 }
 
-navSlide();
+console.log(add(4, 6))
